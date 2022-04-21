e05b14a6ed7b33681ce3813f080f181656abff7a
Martha Scales <mart1c05@stud.kea.dk>
2022-04-21 11:10:05 +0200

    update

--- childtheme/functions.php
+++ childtheme/functions.php
@@ -3,6 +3,7 @@ add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
 function enqueue_parent_styles() {
     /* hent parent stylesheet i parenttemaets mappe */
     wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('parent-style', get_template_directory_uri().'/forside.css');
 
 }
 ?>
