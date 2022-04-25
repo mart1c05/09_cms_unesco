58f172ddfd8d0a7f0b7533411ff930a540a33ef1
Your Name <youremail@yourdomain.com>
2022-04-25 09:34:09 +0200

    projekter css

--- childtheme/page-projekter.php
+++ childtheme/page-projekter.php
@@ -15,12 +15,27 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+
+		<section id="container">
+        <template>
+          <article>
+            <img src="" alt="" />
+            <h3></h3>
+            <p id="aargang"></p>
+            <p id="fag"></p>
+          </article>
+        </template>
+      </section>
+
 		</main><!-- #main -->
 		<h1>TEST FOR HELVEE</h1>
 
 		<script>
-			let projekter =[];
+			let projekter;
 			const fil = "http://marthascales.dk/kea/09_cms/09_cms_unesco-wp/wp-json/wp/v2/projekt";
+
+			const container = document.querySelector("#container");
+			const template = document.querySelector("template");
 
 
 
@@ -33,17 +48,21 @@ get_header();
 			function vis(){
 				console.log(projekter);
 				console.log("hej");
+
+				container.textContent = "";
+
+				projekter.forEach((projekt)=>{
+					const klon = template.cloneNode(true).content;
+					klon.querySelector("img").src = projekt.billede.guid;
+					klon.querySelector("h3").textContent = projekt.title.rendered;
+					klon.querySelector("#aargang").textContent = projekt.aargang[0].name;
+					klon.querySelector("#fag").textContent = projekt.fagvalg[0].name;
+					container.appendChild(klon);
+				})
+
 			}
 
-
 			hentData(fil);
-
-
-
-
-
-
-
 
 		</script>
 	</div><!-- #primary -->
